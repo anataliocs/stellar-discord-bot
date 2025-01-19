3bce097d0d3e49b1e9703996623742e742e93fc5
Chris Anatalio <chris.anatalio@stellar.org>
2025-01-19 00:11:41 -0800

    Fix config and code cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DiscordModule } from '@discord-nestjs/core';
-import { DiscordConfigService } from './discord-config.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BotModule } from './bot.module';
+import { GatewayIntentBits } from 'discord.js';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DiscordModule.forRootAsync({
-      useClass: DiscordConfigService,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        token: configService.get('DISCORD_TOKEN'),
+        discordClientOptions: {
+          intents: [GatewayIntentBits.Guilds],
+        },
+      }),
     }),
+    BotModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
