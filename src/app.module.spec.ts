20da0236d97116ae43b57ad9990321a223f2314c
chris.anatalio <chris.anatalio@stellar.org>
2025-03-19 17:14:32 -0700

    Fix test

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from './app.module';
 import { AppController } from './app.controller';
-import { AuthService } from './auth/auth.service';
 import { UsersService } from './users/users.service';
 import { BotGateway } from './bot/bot.gateway';
 import { ConfigService } from '@nestjs/config';
@@ -41,11 +40,6 @@ describe('AppModule Integration', () => {
   it('should provide AppController', () => {
     const appController = appModule.get<AppController>(AppController);
     expect(appController).toBeDefined();
-  });
-
-  it('should provide AuthService', () => {
-    const authService = appModule.get<AuthService>(AuthService);
-    expect(authService).toBeDefined();
   });
 
   it('should provide UsersService', () => {
